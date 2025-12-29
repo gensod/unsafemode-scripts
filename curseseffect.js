ee5520b5243c016f26640fa3e7bc28d5eae79f1e
Prankapple <bnnaadcscs@gmail.com>
2025-12-29 20:51:54 +0200

    Modify startFlashLoop to include QR image changes
    Updated the flashing loop to change the QR image source during the loop.

--- curseseffect.js
+++ curseseffect.js
@@ -1,14 +1,19 @@
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+// Get the image element by its ID
+const qrImage = document.getElementById('qr');
+
 
 async function startFlashLoop() {
   while (true) {
     bsod.style.background = "#000000";
     bsod.style.color = "#ffffff";
+    qrImage.src = 'https://raw.githubusercontent.com/pcdelatara/pcdelatara.github.io/refs/heads/main/qr.png';
     await sleep(1000);
     bsod.style.background = "#ffffff";
     bsod.style.color = "#000000";
+    qrImage.src = 'https://raw.githubusercontent.com/gensod/unsafemode-scripts/refs/heads/main/src/10Hi.png';
     await sleep(1000);
   }
 }
